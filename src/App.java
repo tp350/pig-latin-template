6e66e98ee1d8ad8eb3cff0aedd699951cf46792b
Mengjiao Cyr <mcyr@pleasantonusd.net>
2024-11-05 04:32:20 +0000

    Template for Pig-latin project.

--- src/App.java
+++ src/App.java
@@ -1,24 +1,42 @@
-import java.util.ArrayList;
-
 public class App {
     public static void main(String[] args)
     {
-        // Variable to store the songs
-        ArrayList<Song> songs;
+        /*  Introduction and TODO List
 
-        // import the songs from the CSV using the SongImporter
-        songs = SongImporter.importSongsFromCSV("songs.csv");
+        This project is setup with four main classes:
+            1. App - the main application
+            2. Book - a class that creates the basic book objects.
+                - a Book contains a representation of a real book.
+                - Book has methods to read data in from various sources
+                - Book also has methods to read data out.
+            3. PigLatinTranslator - a static class
+                - Used to implement your translator.
+                - Has two public methods to take input and return a translated copy.
+                    - Book translate(Book input)
+                    - String translate(String input)
+            4. TestSuite - a simple class that helps you test your work.
+                - Just like CodingBat this class tries your code against various cases.
+                - It will tell you which cases return expected output or not
+         */
 
-        // Loop across all the songs imported
-        for (int i=0; i<songs.size(); i++)
-        {
-            // Get the current song matching the loop index i
-            Song s = songs.get(i);
 
-            // Example - print the song
-            // You should add code to compute song statistics here.
-            System.out.println("Artist:" + s.getArtist());
-        }       
+        // Run tests, comment out once they pass.
+        TestSuite.run();
+
+        // Starter book
+        Book input = new Book();
+
+        // Start with a "test" book based on a string.
+        // Get this to work, and all the tests to pass first.
+        input.readFromString("Test", "Dog\nCat\nMouse");
+
+        // Example reading from a URL
+        //input.readFromUrl("Romeo and Juliette", "https://gutenberg.pglaf.org/cache/epub/1513/pg1513.txt");
+
+        input.printlines(0,2);
+        Book output = PigLatinTranslator.translate(input);
+        output.printlines(0,2);
+        //output.writeToFile();
     }
 }
 
